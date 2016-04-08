11da2b475fec2f569981d18d1e89e0fa0836c5b1
Janosh Björkman <janosh.b@bluewin.ch>
2016-04-08 14:20:29 +0200

    Turnier hinzufügen
    Turnier kann jetzt hinzugefügt werden

--- mlg_soccer_betsite/js/script.js
+++ mlg_soccer_betsite/js/script.js
@@ -1,5 +1,28 @@
+
+$('#turnierErstellenCreateForm').submit(function(e){
+  e.preventDefault();
+  var name = $('#turnierNameEingeben').val();
+  var jahr = $('#turnierJahrEingeben').val();
+  var typ = $('#turnierTypEingeben :selected').text();
+  $.ajax({
+    type: "POST",
+    url: "./insertTurnier.php",
+    data: {name:name,jahr:jahr,typ:typ},
+    success: function(data)
+    {
+      console.log("insertTurnier ok");
+      $("#btnturnierErstellen").click();
+    },
+    error: function(data){
+      console.log("insertTurnier totally failed");
+    }
+  });
+});
+
+
 $(document).ready(function(){
 
+  $('#homeContent').ready(function(){
     $.ajax({
       url:"./readAktiveTurniere.php",
       data: "",
@@ -12,20 +35,23 @@ $(document).ready(function(){
         var turnierTypID = data[4];
         if(!turnierName == '')
         {
-          $('#aktivesTurnier').html("<h1>"+turnierName+"</h1>");
+          $('#aktivesTurnier').hide();
+          $('#aktivesTurnier').html("<h4>Die "+turnierName+" beginnt bald, beginne jetzt zu Wetten!</h4>");
+          $('#aktivesTurnier').fadeIn(800);
         }
       },
       error:function(data){
-        alert('readAktiveTurniere does not work.');
+        console.log('readAktiveTurniere does not work.');
       },
       beforeSend:function(){
         // macht, dass die Tabelle vor dem senden geleert wird
         $("#aktivesTurnier").html = ("");
       }
     });
+  });
 
 
-    $('#userContent').ready(function(){
+    $('#welcomeMessage').ready(function(){
         $.ajax({
           url:"./getLoggedName.php",
           data: "",
@@ -34,7 +60,9 @@ $(document).ready(function(){
             var userVorname = data;
             if(!userVorname == '')
             {
-              $('#userContent').html("<h3>Hallo "+userVorname+"</h3>");
+              $('#welcomeMessage').hide();
+              $('#welcomeMessage').html("<h3>Hallo "+userVorname+".</h3>");
+              $('#welcomeMessage').fadeIn(1000).fadeOut(1000);
             }
           }
         });
